918c593230f545b65365b9e649673609802e0ada
elihudiaz <diazelihu96@gmail.com>
2023-10-19 16:49:48 -0400

    Adding my vec struct

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,27 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use std::ptr::NonNull;
+
+pub struct MyVec<T> {
+    ptr: NonNull<T>,
+    len: usize,
+    capacity: usize,
+}
+
+impl<T> MyVec<T> {
+    pub fn new() -> Self {
+        Self {
+            ptr: NonNull::dangling(),
+            len: 0,
+            capacity: 0,
+        }
+    }
+
+    pub fn capacity(&self) -> usize {
+        self.capacity
+    }
+
+    pub fn len(&self) -> usize {
+        self.len
+    }
 }
 
 #[cfg(test)]
