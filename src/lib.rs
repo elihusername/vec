b224bd92baad9705f980371ecc0eaee89c8ed489
elihudiaz <diazelihu96@gmail.com>
2023-10-20 16:08:40 -0400

    Adding is_empty method

--- src/lib.rs
+++ src/lib.rs
@@ -22,6 +22,16 @@ impl<T> MyVec<T> {
     pub fn len(&self) -> usize {
         self.len
     }
+
+    pub fn is_empty(&self) -> bool {
+        self.len == 0
+    }
+}
+
+impl<T> Default for MyVec<T> {
+    fn default() -> Self {
+        Self::new()
+    }
 }
 
 #[cfg(test)]
@@ -30,13 +40,14 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let mut vec: Vec<usize> = Vec::new();
+        let mut vec = MyVec::<usize>::new();
 
-        vec.push(1usize);
-        vec.push(1);
-        vec.push(2);
+        // vec.push(1usize);
+        // vec.push(1);
+        // vec.push(2);
 
         assert_eq!(vec.capacity(), 4);
         assert_eq!(vec.len(), 3);
+        println!("")
     }
 }
